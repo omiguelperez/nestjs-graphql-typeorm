90a436f73150b5b7dcf8f497ab37e8135c224d32
Oscar Pérez <mr.omiguelperez@gmail.com>
2024-01-28 19:26:35 -0500

    adding validatins with class-validator

--- src/posts/dtos/create-post.input.ts
+++ src/posts/dtos/create-post.input.ts
@@ -1,10 +1,23 @@
 import { InputType, Field } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
 
 @InputType()
 export class CreatePostInput {
+  @MaxLength(100, {
+    message: 'Title is too long',
+  })
+  @IsNotEmpty({
+    message: "Title can't be empty",
+  })
+  @IsString({
+    message: 'Title is required',
+  })
   @Field()
   title: string;
 
+  @MaxLength(400)
+  @IsOptional()
+  @IsString()
   @Field({ nullable: true })
   content?: string;
 }
